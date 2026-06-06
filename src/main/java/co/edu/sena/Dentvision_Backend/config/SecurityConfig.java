0bc7e3f809d32b4d328140d8e1dba3415d3c51ba
christian reyes <creyese23@gmail.com>
2026-06-06 09:07:46 -0500

    Actualizacion backend proyecto Date: 06-06-2026 Time: 09:07

--- src/main/java/co/edu/sena/Dentvision_Backend/config/SecurityConfig.java
+++ src/main/java/co/edu/sena/Dentvision_Backend/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
 
     /**
      * Orígenes permitidos. Se inyectan desde .env / variables de entorno.
-     * Ejemplo en .env: CORS_ALLOWED_ORIGINS=http://localhost:5173,https://miapp.com
+     * Ejemplo en .env: CORS_ALLOWED_ORIGINS=
      */
     @Value("${cors.allowed-origins:*")
     private List<String> allowedOrigins;
